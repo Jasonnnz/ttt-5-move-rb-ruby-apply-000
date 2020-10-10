4f271ecd9d4333fe17d939ca8fdc40f238a29150
bustling-procedure-0781 <jzhou35@buffalo.edu>
2020-10-10 00:28:40 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,8 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(position)
+
+
+def move(board,position,character = "X")
+  board[position] = character
